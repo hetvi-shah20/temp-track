b72422be47e1d8b36e0813a3f579245caf37025c
hetvi-shah20 <hetvishah20898@gmail.com>
2025-03-03 15:33:49 -0500

    Cloud sql scenario integrated

--- src/main/java/com/example/gcpdemo/controller/WeatherController.java
+++ src/main/java/com/example/gcpdemo/controller/WeatherController.java
@@ -12,7 +12,6 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/weather")
